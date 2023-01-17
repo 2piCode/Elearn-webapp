1f1bf741d108afe7aced0b7845bade13e10b0b62
I12 <i.tupikov12@gmail.com>
2023-01-18 03:03:55 +0500

    Geography page

--- Elearn_Webapp/vacancy/urls.py
+++ Elearn_Webapp/vacancy/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('relevance_load', views.relevance_load, name='relevance'),
+    path('relevance_load', views.relevance_load, name='relevance_load'),
     path('relevance', views.relevance, name='relevance'),
+    path('geography_load', views.geography_load, name='geography_load'),
     path('geography', views.geography, name='geography'),
     path('skills', views.skills, name='skills'),
     path('last_vacancies', views.last_vacancies, name='last')
--- Elearn_Webapp/vacancy/views.py
+++ Elearn_Webapp/vacancy/views.py
@@ -12,7 +12,7 @@ def index(request):
 
 
 def relevance_load(request):
-    profession_name = "c++ разработчик"
+    profession_name = "С++ разработчик"
     name_filter = Q(name__icontains=f'{profession_name}') | Q(name__icontains='c++')
     header_year = ["Год", "Средняя зарплата", f"Средняя зарплата - {profession_name}", "Количество вакансий",
                    f"Количество вакансий - {profession_name}"]
@@ -37,6 +37,26 @@ def relevance_load(request):
 def relevance(request):
     return render(request, 'vacancy/relevance.html')
 
+
+def geography_load(request):
+    profession_name = "C++ разработчик"
+    header = ["Город", "Всего вакансий", "Средняя зарплата", f"Вакансий {profession_name}"]
+
+    prof_filter = Q(name__icontains=f'{profession_name}') | Q(name__icontains='с++')
+    prof_count = Count('id', filter=prof_filter)
+    statistics_by_cities = list(Vacancy.objects
+                                .values('area_name')
+                                .annotate(total_count=Count('id'),
+                                            avg_salary=Avg('salary'),
+                                            prof_count=prof_count)
+                                .values('area_name', 'total_count', 'avg_salary', 'prof_count')
+                                .order_by('-prof_count'))
+    data = {
+        "profession_name": profession_name,
+        "header_city": header,
+        "statistics_by_cities": statistics_by_cities
+    }
+    return render(request, 'vacancy/geography.html', data)
 
 def geography(request):
     return render(request, 'vacancy/geography.html')
