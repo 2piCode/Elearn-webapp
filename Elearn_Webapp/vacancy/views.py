500df961d2a7d0b4a1cf1573fa5c290f4da75b29
I12 <i.tupikov12@gmail.com>
2023-01-20 02:02:04 +0500

    clear code

--- Elearn_Webapp/vacancy/views.py
+++ Elearn_Webapp/vacancy/views.py
@@ -3,6 +3,7 @@ from django.db.models import Q, Count, Avg
 from .models import Vacancy
 from collections import Counter
 
+
 def vacancy_home(request):
     vacancies = Vacancy.objects.all()
     return render(request, 'vacancy/vacancy_home.html', {'vacancies': vacancies})
@@ -15,8 +16,6 @@ def index(request):
 def relevance_load(request):
     profession_name = "С++ разработчик"
     name_filter = Q(name__icontains=f'{profession_name}') | Q(name__icontains='c++')
-    header_year = ["Год", "Средняя зарплата", f"Средняя зарплата - {profession_name}", "Количество вакансий",
-                   f"Количество вакансий - {profession_name}"]
     count_by_profession_name = Count('id', filter=name_filter)
     salary_by_profession_name = Avg('salary', filter=name_filter)
     statistics_by_years = list(Vacancy.objects
@@ -28,12 +27,11 @@ def relevance_load(request):
                                        'count_by_profession_name', 'salary_by_profession_name')
                                .order_by())
     data = {
-        'header_year': header_year,
-        'profession_name': f"{profession_name}",
         'statistics_by_years': statistics_by_years
     }
 
     return render(request, 'vacancy/relevance_load.html', data)
+
 
 def relevance(request):
     return render(request, 'vacancy/relevance.html')
@@ -41,8 +39,6 @@ def relevance(request):
 
 def geography_load(request):
     profession_name = "C++ разработчик"
-    header = ["Город", "Всего вакансий", "Средняя зарплата", f"Вакансий {profession_name}"]
-
     name_filter = Q(name__icontains=f'{profession_name}') | Q(name__icontains='с++')
     count_by_profession_name = Count('id', filter=name_filter)
     statistics_by_cities = list(Vacancy.objects
@@ -53,11 +49,10 @@ def geography_load(request):
                                 .values('area_name', 'total_count', 'avg_salary', 'count_by_profession_name')
                                 .order_by('-count_by_profession_name'))
     data = {
-        "profession_name": profession_name,
-        "header_city": header,
         "statistics_by_cities": statistics_by_cities
     }
     return render(request, 'vacancy/geography.html', data)
+
 
 def geography(request):
     return render(request, 'vacancy/geography.html')
@@ -66,13 +61,13 @@ def geography(request):
 def skills(request):
     return render(request, 'vacancy/skills.html')
 
+
 def skills_load(request):
     profession_name = "C++ разработчик"
     name_filter = Q(name__icontains=f'{profession_name}') | Q(name__icontains='с++')
-    vanacies_by_profession = Vacancy.objects.filter(name_filter)
-    
-    header = ["Год", 'Навыки']
-    all_skills = vanacies_by_profession.exclude(key_skills=None).values('key_skills', 'published_at')
+    vancanies_by_profession = Vacancy.objects.filter(name_filter)
+
+    all_skills = vancanies_by_profession.exclude(key_skills=None).values('key_skills', 'published_at')
     skills_by_year = {}
     for skill in all_skills:
         year = skill["published_at"]
@@ -85,8 +80,7 @@ def skills_load(request):
         c = Counter(skills)
         skills_by_year[year] = [(name, c[name] / len(skills) * 100.0) for name, count in c.most_common(10)]
     data = {
-        "skills_by_year": dict(sorted(skills_by_year.items(), reverse=True)),
-        'headers': header
+        "skills_by_year": dict(sorted(skills_by_year.items(), reverse=True))
     }
     return render(request, 'vacancy/skills_load.html', data)
 
